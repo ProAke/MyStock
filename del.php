8f8c0d543ffdd0d551148bec8ad17d6170272f13
Kingeru 'Ake Worapot Bhilabutra <worapot.kingeru@gmail.com>
2021-05-26 07:20:17 +0700

    del.php fix bug

--- del.php
+++ del.php
@@ -3,15 +3,13 @@
 include_once("./include/config.inc.php");
 include_once("./include/function.inc.php");
 include_once("./include/class.inc.php");
-include_once("./include/class.TemplatePower.inc.php");
-
 
 
 $barcode = isset($_GET['barcode']) ? $_GET['barcode'] : '';
 $action = isset($_GET['action']) ? $_GET['action'] : '';
 $id = isset($_GET['id']) ? $_GET['id'] : '';
 
-if($action=='del' && !isset($barcode){
+if($action=='del' && !isset($id){
 
 
 	$query = "DELETE FROM `$tableBarcode` WHERE `ID`='".$id."'";
